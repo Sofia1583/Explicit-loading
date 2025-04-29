8396b920e32ba5e8016e5ac63dfc430000e76f6d
Unknown <unknown@gmail.com>
2025-04-29 15:03:17 +0500

    добавлены разные способы добавления

--- Explicit loading/Program.cs
+++ Explicit loading/Program.cs
@@ -21,12 +21,46 @@ using (ApplicationContext db = new ApplicationContext())
 
     db.SaveChanges();
 }
+//метод с использованием Load
+using (ApplicationContext db = new ApplicationContext())
+{
+    Company? company = db.Companies.FirstOrDefault();
+    if (company != null)
+    {
+        db.Users.Where(u => u.CompanyId == company.Id).Load();
+
+        Console.WriteLine($"Company: {company.Name}");
+        foreach (var u in company.Users)
+            Console.WriteLine($"User: {u.Name}");
+    }
+}
+//с использованием метода Collection
+using (ApplicationContext db = new ApplicationContext())
+{
+    Company? company = db.Companies.FirstOrDefault();
+    if (company != null)
+    {
+        db.Entry(company).Collection(c => c.Users).Load();
+
+        Console.WriteLine($"Company: {company.Name}");
+        foreach (var u in company.Users)
+            Console.WriteLine($"User: {u.Name}");
+    }
+}
+//с использованием метода Reference
+using (ApplicationContext db = new ApplicationContext())
+{
+    User? user = db.Users.FirstOrDefault();  // получаем первого пользователя
+    if (user != null)
+    {
+        db.Entry(user).Reference(u => u.Company).Load();
+        Console.WriteLine($"{user.Name} - {user.Company?.Name}");   // Tom - Microsoft
+    }
+}
+//обращение с использованием Local
 using (ApplicationContext db = new ApplicationContext())
 {
     Company? company1 = db.Companies.Find(1);
-    db.Users.Where(u => u.CompanyId == company1.Id).Load();
-    
-
     if (company1 != null)
     {
         db.Users.Where(u => u.CompanyId == company1.Id).Load();
@@ -34,7 +68,6 @@ using (ApplicationContext db = new ApplicationContext())
     }
 
     Company? company2 = db.Companies.Find(2);
-    db.Users.Where(u => u.CompanyId == company2.Id).Load();
     if (company2 != null)
     {
         db.Users.Where(u => u.CompanyId == company2.Id).Load();
